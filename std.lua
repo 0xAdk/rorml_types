9a75c012f29091d767355dc77aad0529902c4ed9
0xadk <0xadk@users.noreply.github.com>
2023-07-26 12:42:42 -0700

    feat: add extra std table and math functions

---
+++ std.lua
@@ -0,0 +1,57 @@
+---@meta _
+
+--- TODO
+---
+---@generic T
+---@param t T[] TODO
+---@return T
+function table.irandom(t) end
+
+--- TODO
+---
+---@generic T
+---@param t table<any, T> TODO
+---@return T
+function table.random(t) end
+
+--- TODO
+---
+---@param percent number TODO
+---@return boolean
+function math.chance(percent) end
+
+--- TODO
+---
+---@param value number TODO
+---@param target number TODO
+---@param step? number TODO
+---@return number
+function math.approach(value, target, step) end
+
+--- TODO
+---
+---@param n number TODO
+---@return -1|0|1
+function math.sign(n) end
+
+--- TODO
+---
+---@param n number TODO
+---@return number
+function math.round(n) end
+
+--- TODO
+---
+---@param value number TODO
+---@param lower number TODO
+---@param upper number TODO
+---@return number
+function math.clamp(value, lower, upper) end
+
+--- TODO
+---
+---@param from number TODO
+---@param to number TODO
+---@param amount number TODO
+---@return number
+function math.lerp(from, to, amount) end
