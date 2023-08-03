0902a42ad850e1d15cb0740f0e72ad4a4262227c
0xadk <0xadk@users.noreply.github.com>
2023-08-03 02:37:20 -0700

    feat: add `DamagerInstance` and `ItemInstance` classes

---
+++ damager_instance.lua
@@ -0,0 +1,17 @@
+---@meta _
+
+--- TODO
+---
+---@class DamagerInstance: Instance
+local DamagerInstance = {}
+
+
+--- TODO
+---
+---@return Instance?
+function DamagerInstance:getParent() end
+
+--- TODO
+---
+---@return boolean
+function DamagerInstance:isExplosion() end
---
+++ item_instance.lua
@@ -0,0 +1,12 @@
+---@meta _
+
+--- TODO
+---
+---@class ItemInstance: Instance
+local ItemInstance = {}
+
+
+--- TODO
+---
+---@return Item
+function ItemInstance:getItem() end
