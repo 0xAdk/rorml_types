740e27e685e44a3e72443c044b78e7d029d99045
0xadk <0xadk@users.noreply.github.com>
2023-09-10 09:06:32 -0700

    feat: add docs to Surface and DynamicSprite classes

--- dynamic_sprite.lua
+++ dynamic_sprite.lua
@@ -1,31 +1,45 @@
 ---@meta _
 
---- TODO
+--- The `DynamicSprite` class is used to create [Sprites](https://saturnyoshi.gitlab.io/RoRML-Docs/class/sprite.html) at runtime.
+---
+--- DynamicSprite inherits all functionality from [BaseSprite](https://saturnyoshi.gitlab.io/RoRML-Docs/class/baseSprite.html).
+---
+--- A `DynamicSprite` is created using the [Surface:createSprite](https://saturnyoshi.gitlab.io/RoRML-Docs/class/surface.html#surface-createsprite) method.
+---
+--- Much like a [Surface](https://saturnyoshi.gitlab.io/RoRML-Docs/class/surface.html), a `DynamicSprite` can be created at any time and drawn to the screen.
+--- Unlike a [Surface](https://saturnyoshi.gitlab.io/RoRML-Docs/class/surface.html), a `DynamicSprite` will never stop being valid unless it is explicitly deleted, but does require a [Surface](https://saturnyoshi.gitlab.io/RoRML-Docs/class/surface.html) to be created from.
+--- This class can also be used to generate regular [Sprites](https://saturnyoshi.gitlab.io/RoRML-Docs/class/sprite.html) which can be used
+--- to dynamically generate items or other content.
 ---
 ---@class DynamicSprite: BaseSprite
 local DynamicSprite = {}
 
---- TODO
+--- Deletes the sprite, causing it to become invalid.
+--- All future attempts to access the sprite will result in an error.
 function DynamicSprite:delete() end
 
---- TODO
+--- Checks whether the sprite still exists.
+--- This will only return false after the sprite’s delete method has been called.
 ---
 ---@return boolean
 function DynamicSprite:isValid() end
 
---- TODO
+--- Used to retrieve a final [Sprite](https://saturnyoshi.gitlab.io/RoRML-Docs/class/sprite.html).
+--- Also causes the `DynamicSprite` to become invalid.
 ---
----@param name string TODO
----@return Sprite
+---@param name string The name to give the sprite within the current namespace
+---@return Sprite '' A new Sprite with the same contents
 function DynamicSprite:finalise(name) end
 
+--- Alias for `DynamicSprite.finalise`
 DynamicSprite.finalize = DynamicSprite.finalise
 
---- TODO
+--- Adds a new frame to the sprite, using the provided [Surface](https://saturnyoshi.gitlab.io/RoRML-Docs/class/surface.html).
+--- The frame is appended at the end of the current animation sequence.
 ---
----@param source Surface TODO
----@param x? number TODO
----@param y? number TODO
----@param w? number TODO
----@param h? number TODO
+---@param source Surface The [Surface](https://saturnyoshi.gitlab.io/RoRML-Docs/class/surface.html) to pull the image from
+---@param x? number The left x coordinate of the region of the surface to copy. *defaults to 0*
+---@param y? number The top y coordinate of the region of the surface to copy. *default to 0*
+---@param w? number The width of the region of the surface to copy. *defaults to the width of the sprite*
+---@param h? number The height of the region of the surface to copy. *defaults to the height of the sprite*
 function DynamicSprite:addFrame(source, x, y, w, h) end
--- surface.lua
+++ surface.lua
@@ -1,10 +1,12 @@
 ---@meta _
 
---- TODO
+--- A `Surface` is an image which can be drawn to.
+---
+--- **Note**: Surfaces become invalid when the game is minimized, so you should always make sure a surface is valid before accessing it.
 ---
 ---@class Surface
----@field width number TODO
----@field height number TODO
+---@field width number The width of the surface, in pixels
+---@field height number The height of the surface, in pixels
 ---
 ---@overload fun(width: number, height: number): Surface
 Surface = {}
@@ -13,47 +15,96 @@ Surface = {}
 ---- static functions
 --]]
 
---- TODO
+--- Creates a new Surface
 ---
----@param width number TODO
----@param height number TODO
+--- # Example
+---     Create a new surface with a width of 8 and a height of 256.
+---
+---     ```lua
+---     local vine = Surface.new(8, 256)
+---     ```
+---
+---@param width number The width of the surface, in pixels
+---@param height number The height of the surface, in pixels
 ---@return Surface
 function Surface.new(width, height) end
 
---- TODO
+--- Checks if a value is a valid surface.
 ---
----@param val? any TODO
----@return boolean
+---@param val? any The value to check
+---@return boolean '' true if the value is a surface, and `value:isValid()` is true
 function Surface.isValid(val) end
 
 --[[
 ---- methods
 --]]
 
---- TODO
+--- Manually deletes the surface.
+--- 
+--- Surfaces take up graphical memory which is a limited resource, so it is important to free them when you’re done with them.
+---
+--- # Example
+---     In this code we create a temporary surface and then draw it,
+---     then since we’re not going to need it for next frame, we free it from memory.
+---
+---     ```lua
+---     local tempsurf = createTempSurface()
+---     tempsurf:draw(xpos, ypos)
+---     tempsurf:free()
+---     ```
+---
 function Surface:free() end
 
---- TODO
+--- Checks whether the surface still exists.
 ---
----@return boolean
+--- In some cases using `Surface.isValid` may be preferable.
+--- 
+--- # Example
+---     Check if the surface `vine` is valid before drawing it.
+---     If the surface is not valid, then a function is called to create a new surface.
+---
+---     ```lua
+---     if not vine:isValid() then
+---         vine = constructVineSurface()
+---     end
+---
+---     vine:draw(vineposx, vineposy)
+---     ```
+---
+---@return boolean '' true if surface exists otherwise false
 function Surface:isValid() end
 
---- TODO
+--- Fills the surface with full transparency.
+---
+--- # Example
+---     Removes anything drawn to the surface `vine` so it can be drawn to without overlapping the previous contents.
+---
+---     ```lua
+---     vine:clear()
+---     ```
+---
 function Surface:clear() end
 
---- TODO
+--- Draws the surface at the specified coordinates.
 ---
----@param x number TODO
----@param y number TODO
+--- # Example
+---     Draw the surface `bubble` relative to the position of `player`.
+---
+---     ```lua
+---     bubble:draw(player.x - 10, player.y - 10)
+---     ```
+---
+---@param x number The horizontal position of the top left corner of where to draw the surface, relative to the current drawing surface
+---@param y number The vertical position of the top left corner of where to draw the surface, relative to the current drawing surface
 function Surface:draw(x, y) end
 
---- TODO
+--- Creates a new [DynamicSprite](https://saturnyoshi.gitlab.io/RoRML-Docs/class/dynamicSprite.html), adding the surface as its only subimage.
 ---
----@param xorigin? number TODO
----@param yorigin? number TODO
----@param x? number TODO
----@param y? number TODO
----@param w? number TODO
----@param h? number TODO
----@return DynamicSprite
+---@param xorigin? number The x coordinate of the sprite’s origin. defaults to (TODO)
+---@param yorigin? number The y coordinate of the sprite’s origin. defaults to (TODO)
+---@param x? number The left x coordinate of the region of the surface to copy. *defaults to 0*
+---@param y? number The top y coordinate of the region of the surface to copy. *default to 0*
+---@param w? number The width of the surface. *defaults to the width of the surface*
+---@param h? number The height of the surface. *defaults to the height of the surface*
+---@return DynamicSprite '' The newly created [DynamicSprite](https://saturnyoshi.gitlab.io/RoRML-Docs/class/dynamicSprite.html) object.
 function Surface:createSprite(xorigin, yorigin, x, y, w, h) end
