a1a013cf02b3a0108a1e9cbad174d0c8b8622e8e
0xadk <0xadk@users.noreply.github.com>
2023-09-10 04:43:09 -0700

    feat: add docs to GMObject class

--- gmobject_base.lua
+++ gmobject_base.lua
@@ -1,20 +1,41 @@
 ---@meta _
 
---- TODO
+--- The BaseSprite class provides shared methods to the [GMObject](https://saturnyoshi.gitlab.io/RoRML-Docs/class/gmObject.html) and [ParentObject](https://saturnyoshi.gitlab.io/RoRML-Docs/class/parentObject.html) classes.
+---
+--- Note: Methods which return an [Instance](https://saturnyoshi.gitlab.io/RoRML-Docs/class/instance.html) may return any [Instance](https://saturnyoshi.gitlab.io/RoRML-Docs/class/instance.html) child class depending on the calling object.
 ---
 ---@class GMObjectBase
 local GMObjectBase = {}
 
 
---- TODO
+--- Finds the `n`th [Instance](https://saturnyoshi.gitlab.io/RoRML-Docs/class/instance.html) of the object. Instances are sorted by the order they were created.
 ---
----@param n number TODO
----@return Instance?
+--- # Example
+---     Print the IDs of the first 5 instances of the `eggplant` object.
+---
+---     ```lua
+---     for i = 1, 5 do
+---         local instance = eggplant:find(i)
+---         print(instance.ID)
+---     end
+---     ```
+---
+---@param n number The number of the instance to find.
+---@return Instance? '' The [Instance](https://saturnyoshi.gitlab.io/RoRML-Docs/class/instance.html) if found, otherwise nil.
 function GMObjectBase:find(n) end
 
---- TODO
+--- Finds all [Instances](https://saturnyoshi.gitlab.io/RoRML-Docs/class/instance.html) of the object.
 ---
----@return Instance[]
+--- # Example
+---     Destroy all instances of the object `eggplant`.
+---
+---     ```lua
+---     for _, instance in ipairs(eggplant:findAll()) do
+---         instance:destroy()
+---     end
+---     ```
+---
+---@return Instance[] '' A numerically indexed table containing all the found [Instances](https://saturnyoshi.gitlab.io/RoRML-Docs/class/instance.html).
 function GMObjectBase:findAll() end
 
 --- TODO
@@ -22,12 +43,24 @@ function GMObjectBase:findAll() end
 ---@return number _ TODO correct type?
 function GMObjectBase:count() end
 
---- TODO
+--- Finds all [Instances](https://saturnyoshi.gitlab.io/RoRML-Docs/class/instance.html) of the object with matching key and value variable pairs.
 ---
----@param key string TODO
----@param value string|number|nil TODO
----@param ... string|number|nil TODO
----@return Instance[]
+--- Example:
+---     Heal all Lemurians where `boost` is nil by 50 health,
+---     and then sets `boost` to 1 so they’re only healed once.
+---
+---     ```lua
+---     local lizard = Object.find("lizard", "vanilla")
+---     for _, v in ipairs(lizard:findMatching("boost", nil) do
+---         v:set("hp", v:get("hp") + 50)
+---         v:set("boost", 1)
+---     end
+---     ```
+---
+---@param key string The name of the first variable to match.
+---@param value string|number|nil The value of the first variable to match.
+---@param ... string|number|nil Any number of additional key and value pairs may be specified.
+---@return Instance[] '' A numerically indexed table containing all the matching [Instances](https://saturnyoshi.gitlab.io/RoRML-Docs/class/instance.html).
 function GMObjectBase:findMatching(key, value, ...) end
 
 ---@alias FindMatchingOperator
@@ -38,93 +71,117 @@ function GMObjectBase:findMatching(key, value, ...) end
 ---| '<='
 ---| '>='
 
---- TODO
+--- Finds all [Instances](https://saturnyoshi.gitlab.io/RoRML-Docs/class/instance.html) of the object with matching key and value variable pairs.
 ---
----@param key string TODO
----@param operator FindMatchingOperator TODO
----@param value string|number|nil TODO
----@param ... string|number|nil|FindMatchingOperator TODO
----@return Instance[]
+--- # Example
+---     Kill all Lemurians with less than 30 remaining health.
+---
+---     ```lua
+---     local lizard = Object.find("lizard", "vanilla")
+---     for _, v in ipairs(lizard:findMatchingOp("hp", "<", 30) do
+---         v:kill()
+---     end
+---     ```
+---
+---@param key string The name of the first variable to compare against.
+---@param operator FindMatchingOperator The operator to compare with.
+---@param value string|number|nil The value of the first variable to compare against.
+---@param ... string|number|nil|FindMatchingOperator Any number of additional key, operator and value sets may be specified.
+---@return Instance[] '' A numerically indexed table containing all the matching [Instances](https://saturnyoshi.gitlab.io/RoRML-Docs/class/instance.html).
 function GMObjectBase:findMatchingOp(key, operator, value, ...) end
 
---- TODO
+--- Finds the [Instance](https://saturnyoshi.gitlab.io/RoRML-Docs/class/instance.html) of the object closest to a point.
 ---
----@param x number TODO
----@param y number TODO
----@return Instance
+--- # Example
+---     Find the instance of the object `eggplant` closest to the instance `player`.
+---
+---     ```lua
+---     local target = eggplant:findNearest(player.x, player.y)
+---     ```
+---
+---@param x number The horizontal coordinate of the point to check from.
+---@param y number The vertical coordinate of the point to check from.
+---@return Instance '' The [Instance](https://saturnyoshi.gitlab.io/RoRML-Docs/class/instance.html) if found, otherwise nil.
 function GMObjectBase:findNearest(x, y) end
 
---- TODO
+--- Finds the [Instance](https://saturnyoshi.gitlab.io/RoRML-Docs/class/instance.html) of the object furthest from a point.
 ---
----@param x number TODO
----@param y number TODO
----@return Instance
+--- # Example
+---     Find the instance of the object `eggplant` farthest from the instance `player`.
+---
+---     ```lua
+---     local target = eggplant:findFurthest(player.x, player.y)
+---     ```
+---
+---@param x number The horizontal coordinate of the point to check from.
+---@param y number The vertical coordinate of the point to check from.
+---@return Instance '' The [Instance](https://saturnyoshi.gitlab.io/RoRML-Docs/class/instance.html) if found, otherwise nil.
 function GMObjectBase:findFurthest(x, y) end
 
---- TODO
+--- Finds a single [Instance](https://saturnyoshi.gitlab.io/RoRML-Docs/class/instance.html) inside of the specified rectangle.
 ---
----@param x1 number TODO
----@param y1 number TODO
----@param x2 number TODO
----@param y2 number TODO
----@return Instance
+---@param x1 number First x coordinates of the rectangle to check.
+---@param y1 number First y coordinates of the rectangle to check.
+---@param x2 number Second x coordinates of the rectangle to check.
+---@param y2 number Second y coordinates of the rectangle to check.
+---@return Instance '' The first [Instance](https://saturnyoshi.gitlab.io/RoRML-Docs/class/instance.html) found within the rectangle.
 function GMObjectBase:findRectangle(x1, y1, x2, y2) end
 
---- TODO
+--- Finds all [Instances](https://saturnyoshi.gitlab.io/RoRML-Docs/class/instance.html) inside of the specified rectangle.
 ---
----@param x1 number TODO
----@param y1 number TODO
----@param x2 number TODO
----@param y2 number TODO
----@return Instance[]
+---@param x1 number First x coordinates of the rectangle to check.
+---@param y1 number First y coordinates of the rectangle to check.
+---@param x2 number Second x coordinates of the rectangle to check.
+---@param y2 number Second y coordinates of the rectangle to check.
+---@return Instance[] '' A numerically indexed table of [Instances](https://saturnyoshi.gitlab.io/RoRML-Docs/class/instance.html) found within the rectangle.
 function GMObjectBase:findAllRectangle(x1, y1, x2, y2) end
 
---- TODO
+--- Finds a single [Instance](https://saturnyoshi.gitlab.io/RoRML-Docs/class/instance.html) inside of the specified ellipse.
 ---
----@param x1 number TODO
----@param y1 number TODO
----@param x2 number TODO
----@param y2 number TODO
----@return Instance
+---@param x1 number First x coordinates of the ellipse bounding box to check.
+---@param y1 number First y coordinates of the ellipse bounding box to check.
+---@param x2 number Second x coordinates of the ellipse bounding box to check.
+---@param y2 number Second y coordinates of the ellipse bounding box to check.
+---@return Instance '' The first [Instance](https://saturnyoshi.gitlab.io/RoRML-Docs/class/instance.html) found within the ellipse.
 function GMObjectBase:findEllipse(x1, y1, x2, y2) end
 
---- TODO
+--- Finds all [Instances](https://saturnyoshi.gitlab.io/RoRML-Docs/class/instance.html) inside of the specified ellipse.
 ---
----@param x1 number TODO
----@param y1 number TODO
----@param x2 number TODO
----@param y2 number TODO
----@return Instance[]
+---@param x1 number First x coordinates of the ellipse bounding box to check.
+---@param y1 number First y coordinates of the ellipse bounding box to check.
+---@param x2 number Second x coordinates of the ellipse bounding box to check.
+---@param y2 number Second y coordinates of the ellipse bounding box to check.
+---@return Instance[] '' A numerically indexed table of [Instances](https://saturnyoshi.gitlab.io/RoRML-Docs/class/instance.html) found within the ellipse.
 function GMObjectBase:findAllEllipse(x1, y1, x2, y2) end
 
---- TODO
+--- Finds a single [Instance](https://saturnyoshi.gitlab.io/RoRML-Docs/class/instance.html) colliding with the specified line.
 ---
----@param x1 number TODO
----@param y1 number TODO
----@param x2 number TODO
----@param y2 number TODO
----@return Instance
+---@param x1 number The x coordinate of the first end of the line.
+---@param y1 number The y coordinate of the first end of the line.
+---@param x2 number The x coordinate of the second end of the line.
+---@param y2 number The y coordinate of the second end of the line.
+---@return Instance '' The first [Instance](https://saturnyoshi.gitlab.io/RoRML-Docs/class/instance.html) colliding with the line.
 function GMObjectBase:findLine(x1, y1, x2, y2) end
 
---- TODO
+--- Finds all [Instances](https://saturnyoshi.gitlab.io/RoRML-Docs/class/instance.html) colliding with the specified line.
 ---
----@param x1 number TODO
----@param y1 number TODO
----@param x2 number TODO
----@param y2 number TODO
----@return Instance[]
+---@param x1 number The x coordinate of the first end of the line.
+---@param y1 number The y coordinate of the first end of the line.
+---@param x2 number The x coordinate of the second end of the line.
+---@param y2 number The y coordinate of the second end of the line.
+---@return Instance[] '' A numerically indexed table of [Instances](https://saturnyoshi.gitlab.io/RoRML-Docs/class/instance.html) colliding with the line.
 function GMObjectBase:findAllLine(x1, y1, x2, y2) end
 
---- TODO
+--- Finds a single [Instance](https://saturnyoshi.gitlab.io/RoRML-Docs/class/instance.html) colliding with the specified point.
 ---
----@param x number TODO
----@param y number TODO
----@return Instance
+---@param x number The x coordinate of the point to check.
+---@param y number The y coordinate of the point to check.
+---@return Instance '' The first [Instance](https://saturnyoshi.gitlab.io/RoRML-Docs/class/instance.html) colliding with the point.
 function GMObjectBase:findPoint(x, y) end
 
---- TODO
+--- Finds all [Instances](https://saturnyoshi.gitlab.io/RoRML-Docs/class/instance.html) colliding with the specified point.
 ---
----@param x number TODO
----@param y number TODO
----@return Instance[]
+---@param x number The x coordinate of the point to check.
+---@param y number The y coordinate of the point to check.
+---@return Instance[] '' A numerically indexed table of [Instances](https://saturnyoshi.gitlab.io/RoRML-Docs/class/instance.html) colliding with the point.
 function GMObjectBase:findAllPoint(x, y) end
